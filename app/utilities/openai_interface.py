58a56987352893017ba3bc6e27937233033f8d03
Koffi Anderson Koffi <kandersonko@gmail.com>
2024-07-23 07:10:55 -0600

    Add dspy model and routing based on context length

--- app/utilities/openai_interface.py
+++ app/utilities/openai_interface.py
@@ -1,12 +1,20 @@
 import os
+from pathlib import Path
 import openai
 import chardet
 from PyPDF2 import PdfReader
+from app.utilities.prompt_optimization import dspy_model
+
+# 128K is the max context length for the GPT-4o model
+# we use less than this to be safe
+max_context_length = 90000
+
 
 def detect_encoding(file_path):
-        with open(file_path, 'rb') as file:
-            result = chardet.detect(file.read())
-        return result['encoding']
+    with open(file_path, "rb") as file:
+        result = chardet.detect(file.read())
+    return result["encoding"]
+
 
 def extract_text_from_pdf(pdf_path):
     reader = PdfReader(pdf_path)
@@ -15,6 +23,8 @@ def extract_text_from_pdf(pdf_path):
         text += page.extract_text()
     return text
 
+
+# TODO we might need to rename the class
 class OpenAIInterface:
     loaded_doc = ""
 
@@ -27,34 +37,78 @@ class OpenAIInterface:
         prompt = ""
         print("asking question")
         if len(item.text_blocks) > 0:
-            prompt = """Given the following document, and the attached additional context, answer the following question. Return the result as nicely formatted html.\nQuestion:\n""" + item.searchphrase 
-            
+            prompt = (
+                """Given the following document, and the attached additional context, answer the following question. Return the result as nicely formatted html.\nQuestion:\n"""
+                + item.searchphrase
+            )
+
             for block in item.text_blocks:
                 prompt += "\n\nContext:\n" + block
-            
+
             prompt += "\n\nDocument:\n" + self.loaded_doc
-            print(prompt)
+            # print(prompt)
         else:
             print("no text blocks")
-            prompt = """Given the following document, answer the following question. Return the result as nicely formatted html.:\nQuestion:\n""" + item.searchphrase + "\Document:\n" + self.loaded_doc
-        
-        completion = openai.chat.completions.create(model="gpt-4o", 
-                                              messages=[{"role": "user", "content": prompt}],
-                                             )
+            prompt = (
+                """Given the following document, answer the following question. Return the result as nicely formatted html.:\nQuestion:\n"""
+                + item.searchphrase
+                + "\Document:\n"
+                + self.loaded_doc
+            )
+
+        completion = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": prompt}],
+        )
         return completion.choices[0].message.content
-    
+
     def ask_question_to_documents(self, root_path, documents, question):
 
         full_text = ""
         for document in documents:
             full_path = os.path.join(root_path, "static", "uploads", document.path)
             full_text += "\n\nDocument:" + extract_text_from_pdf(full_path) + " "
-        
+
+        # TODO add the dspy model here (route based on the length of the documents, if larger call dspy model, if smaller call the chat model)
+
         openai.api_key = "sk-proj-Tdb51ojrv5lwDtPH9S3tT3BlbkFJ6ty7hYO3Ow8weqXu6UjM"
-        prompt = """Given the following document(s), answer the following question. Return the result as nicely formatted html with supportive information as if to display in a web interface chat bot. The html tags should fit nicely in a div on the page and not break formatting. Question:""" + question + "\n" + full_text
-        completion = openai.chat.completions.create(model="gpt-4o", 
-                                              messages=[{"role": "user", "content": prompt}],
-                                             )
-        return completion.choices[0].message.content
-    
-    
+        prompt = (
+            """Given the following document(s), answer the following question. Return the result as nicely formatted html with supportive information as if to display in a web interface chat bot. The html tags should fit nicely in a div on the page and not break formatting. Question:"""
+            + question
+            + "\n"
+            + full_text
+        )
+        total_context_length = len(prompt) + len(full_text)
+
+        print("total context length: ", total_context_length)
+        if total_context_length > max_context_length:
+            print("using dspy model")
+            print("question: ", question)
+
+            # TODO call the dspy model here
+            # return "The document(s) are too large to process, please contact support for assistance."
+            persistent_directory = Path(root_path) / "static" / "uploads"
+            collection_name = "chat_dspy_model"
+            model = dspy_model(full_text, collection_name, persistent_directory)
+            response, queries = model(question=question)
+            output_prompt = (
+                (
+                    """Format the following answer to the given question as a nicely formatted html with supportive information to display in a web interface chat bot. The html tags should fit nicely in a div on the page and not break formatting. Do not add ```html before your response. Do not add 'Question' and 'Answer' heading or title in your response, but only respond only with the formatted html code for the answer.\n\n Question: """
+                )
+                + question
+                + "\n\nAnwsers: "
+                + response.answer
+            )
+            print("dspy response: ", response.answer)
+            completion = openai.chat.completions.create(
+                model="gpt-4o",
+                messages=[{"role": "user", "content": output_prompt}],
+            )
+            print("llm formatted response: ", completion.choices[0].message.content)
+            return completion.choices[0].message.content
+        else:
+            completion = openai.chat.completions.create(
+                model="gpt-4o",
+                messages=[{"role": "user", "content": prompt}],
+            )
+            return completion.choices[0].message.content
---
+++ app/utilities/prompt_optimization.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+# from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.schema import Document
+
+# from langchain_openai import OpenAI, ChatOpenAI, OpenAIEmbeddings
+
+import chromadb
+
+import dspy
+
+from dsp.utils import deduplicate
+
+from dspy.retrieve.chromadb_rm import ChromadbRM
+from dspy.evaluate.metrics import (
+    answer_exact_match,
+    answer_exact_match_str,
+    answer_passage_match,
+)
+
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+)
+
+from langchain_community.document_loaders import PyPDFLoader
+
+
+from langchain_openai import OpenAIEmbeddings
+
+
+from pathlib import Path
+import dotenv
+
+
+dotenv.load_dotenv()
+os.environ["OPENAI_API_KEY"] = (
+    "sk-proj-Tdb51ojrv5lwDtPH9S3tT3BlbkFJ6ty7hYO3Ow8weqXu6UjM"
+)
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+def sanitize_filename(filename: str) -> str:
+    # Remove file extension
+    name_without_extension = Path(filename).stem
+
+    # Replace spaces and special characters with underscores
+    sanitized = re.sub(r"[^\w\-_\.]", "_", name_without_extension)
+
+    # Remove leading/trailing underscores
+    sanitized = sanitized.strip("_")
+
+    # Ensure the name starts with a letter or underscore
+    if not sanitized[0].isalpha() and sanitized[0] != "_":
+        sanitized = f"_{sanitized}"
+
+    # Limit the length (Chroma might have a maximum length for collection names)
+    max_length = 63  # Adjust this if Chroma has a different limit
+    sanitized = sanitized[:max_length]
+
+    return sanitized
+
+
+def validate_query_distinction_local(previous_queries, query):
+    """check if query is distinct from previous queries"""
+    if previous_queries == []:
+        return True
+    if answer_exact_match_str(query, previous_queries, frac=0.8):
+        return False
+    return True
+
+
+def validate_context_and_answer_and_hops(example, pred, trace=None):
+    if not answer_exact_match(example, pred):
+        return False
+
+    if not answer_passage_match(example, pred):
+        return False
+
+    return True
+
+
+def all_queries_distinct(prev_queries):
+    query_distinct = True
+    for i, query in enumerate(prev_queries):
+        if validate_query_distinction_local(prev_queries[:i], query) == False:
+            query_distinct = False
+            break
+    return query_distinct
+
+
+class GenerateAnswer(dspy.Signature):
+    """Answer questions based on the provided context."""
+
+    context = dspy.InputField(desc="may contain relevant facts")
+    question = dspy.InputField()
+    answer = dspy.OutputField()
+
+
+class GenerateSearchQuery(dspy.Signature):
+    """Write a simple search query that will help answer a complex question."""
+
+    context = dspy.InputField(desc="may contain relevant facts")
+    question = dspy.InputField(desc="complex question")
+    query = dspy.OutputField(desc="A search query/question to retrieve relevant facts")
+
+
+class MultiHopQAModel(dspy.Module):
+    def __init__(self, passages_per_hop=2, max_hops=2):
+        super().__init__()
+
+        self.generate_query = [
+            dspy.ChainOfThought(GenerateSearchQuery) for _ in range(max_hops)
+        ]
+        self.retrieve = dspy.Retrieve(k=passages_per_hop)
+        # TODO add pappg as the main context
+        # self.main_context =
+        self.generate_answer = dspy.ChainOfThought(GenerateAnswer)
+        self.max_hops = max_hops
+        # self.critic = dspy.ChainOfThought("answer->critic")
+
+        # for evaluating assertions only
+        self.passed_suggestions = 0
+
+    def forward(self, question):
+        context = []
+        prev_queries = [question]
+
+        for hop in range(self.max_hops):
+            query = self.generate_query[hop](context=context, question=question).query
+            # prev_queries.append(query)
+            prev_queries = deduplicate(prev_queries + [query])
+            passages = self.retrieve(query).passages
+            context = deduplicate(context + passages)
+
+        if all_queries_distinct(prev_queries):
+            self.passed_suggestions += 1
+
+        pred = self.generate_answer(context=context, question=question)
+        pred = dspy.Prediction(context=context, answer=pred.answer)
+        return pred, prev_queries
+
+
+# llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
+
+embedding_model = "text-embedding-3-large"
+embedding = OpenAIEmbeddings(model=embedding_model)
+
+
+def get_document_splits(document: str, file_path: Path, persistent_directory: Path):
+    # get the file_path from the user's storage
+
+    loader = PyPDFLoader(file_path.as_posix())
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    return splits
+
+
+def get_retriever(document: str, file_path, persistent_directory: Path):
+    splits = get_document_splits(document, file_path)
+    collection_name = sanitize_filename(document)
+
+    vectordb = Chroma.from_documents(
+        persist_directory=persistent_directory,
+        collection_name=collection_name,
+        documents=splits,
+        embedding=embedding,
+    )
+
+    retriever = vectordb.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 3},
+    )
+    return retriever
+
+
+def dspy_model(
+    full_text: str,
+    collection_name: str,
+    persistent_directory: Path,
+):
+    # model_name = "gpt-4"
+    # model_name = "gpt-4-turbo"
+    model_name = "gpt-4o"
+    # Example questions
+    # "What are the rules around personnel funding?"
+    # "What are the required sections of the proposal?"
+    # "What are the required sections of the proposal and what are their page limits?"
+
+    # pdf_directory = storage_path.as_posix()
+
+    # print("Resolved pdf_directory: ", pdf_directory)
+    # print("Files in directory: ", os.listdir(pdf_directory))
+
+    # loader = DirectoryLoader(pdf_directory, glob="*.pdf", loader_cls=PyPDFLoader)
+
+    # # Load the documents
+    # docs = loader.load()
+
+    # collection_name = sanitize_collection_name(document.filename)
+
+    # docs = [Document(page_content=full_text, metadata={"source": "user_input"})]
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents([Document(page_content=full_text)])
+
+    print("collection_name: ", collection_name)
+
+    # Check if the collection already exists
+
+    chroma_client = chromadb.PersistentClient(path=persistent_directory.as_posix())
+    # existing_collections = chroma_client.list_collections()
+    # collection_exists = any(col.name == collection_name for col in existing_collections)
+
+    # # TODO check if the collection exists using the count of uploaded documents. If the count is different, recreate the collection
+    # if not collection_exists:
+    # print(f"Creating new collection '{collection_name}'")
+    Chroma.from_documents(
+        collection_name=collection_name,
+        documents=docs,
+        persist_directory=persistent_directory.as_posix(),
+        embedding=embedding,
+    )
+
+    rm = ChromadbRM(
+        collection_name=collection_name,
+        persist_directory=persistent_directory.as_posix(),
+        client=chroma_client,
+        embedding_function=embedding.embed_documents,
+        k=3,
+    )
+
+    # model
+    # llm = dspy.OpenAI(model=model_name, max_tokens=max_tokens)
+    llm = dspy.OpenAI(model=model_name, max_tokens=None)
+    dspy.settings.configure(lm=llm, rm=rm, trace=[], temperature=0.7)
+
+    model = MultiHopQAModel(passages_per_hop=3, max_hops=5)
+
+    return model
