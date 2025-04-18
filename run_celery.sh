c7842128cd19ed35303de137a6e037635b0117a3
sheneman <sheneman@uidaho.edu>
2025-04-18 09:13:26 -0700

    nohup and run celery in the background

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-celery -A app.celery_worker.celery_app worker --loglevel INFO
+#celery -A app.celery_worker.celery_app worker --loglevel INFO
+nohup celery -A app.celery_worker.celery_app worker --loglevel INFO > celery.log 2>&1 &
