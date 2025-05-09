3340fa3aaeffbfec84fd9854cc60e536dfc4f5a7
Koffi Anderson Koffi <kandersonko@gmail.com>
2025-05-09 15:55:04 -0600

    feat!: make the extraction manager use the raw text instead of redoing doc ocr

--- app/utilities/extraction_manager3.py
+++ app/utilities/extraction_manager3.py
@@ -6,6 +6,7 @@ from app.utilities.agents import extract_entities_with_agent
 from app.utilities.document_readers import extract_text_from_doc
 from app.utilities.config import model_type
 from app.utilities.llm import ChatLM
+from app.models import SmartDocument
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -13,12 +14,13 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 class ExtractionManager3:
     root_path = ""
 
-    def build_from_documents(self, pdf_paths):
+    def build_from_documents(self, document_uuids):
         openai.api_key = OPENAI_API_KEY
         doc_text = ""
         time.time()
-        for pdf_path in pdf_paths:
-            doc_text += "\n\n" + extract_text_from_doc(doc_path=pdf_path)
+        for document_uuid in document_uuids:
+            doc = SmartDocument.objects(uuid=document_uuid).first()
+            doc_text += doc.raw_text
 
         prompt = (
             """Your job is to build an extraction set from the following information. Take the information given, and the instructions to extract the important information from this text. You will create an array of entities that an LLM could use and faithly reproduce to extract the same values from this text every time. When asked to populate values for the entity types you return, it should give the user the important information from this document every time. Return an array formatted as json with the format {"entities": ["value1", "value2", "etc"]} containing entities for important information in the text. Do not nest values, keep the array flat and one-dimensional. Do not inclued the values, just the entity names in a single array of string values.
@@ -52,7 +54,6 @@ class ExtractionManager3:
         return None
 
     def extract(self, extract_keys, pdf_paths, full_text=None):
-
         # if extract_keys is string convert to list by splitting on comma
         if isinstance(extract_keys, str):
             fields_to_extract = extract_keys.split(",")
