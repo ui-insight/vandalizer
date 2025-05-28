7135b0339520d74616211d94d697eab4781c058e
ViaJables <johnbrunsfeld@gmail.com>
2025-05-28 10:01:41 -0700

    Celery multithread fix

--- run_celery.sh
+++ run_celery.sh
@@ -2,6 +2,7 @@
 
 # on macos celery has some issues pandoc on macos with the default pool, use solo pool to fix it
 # celery -A app.celery_worker.celery_app worker --pool=solo --loglevel INFO
-celery -A app.celery_worker.celery_app worker --loglevel INFO
+celery -A app.celery_worker.celery_app worker -l info -P threads --concurrency=8
+
 
 # nohup celery -A app.celery_worker:celery_app worker --loglevel INFO > celery.log 2>&1 &
