b215f2c65731283d47a5f61501a1b407a7a69138
ViaJables <johnbrunsfeld@gmail.com>
2025-08-27 14:15:24 -0700

    Remove two options from model choices

--- app/utilities/config.py
+++ app/utilities/config.py
@@ -58,18 +58,8 @@ Upload Compliance:
 #         # filter
 
 models = [
-    {"name": "openai/gpt-4.1", "tag": "Cloud — Accurate", "external": True},
-    {
-        "name": "openai/gpt-4.1-nano",
-        "tag": "Cloud — Fast",
-        "external": True,
-    },
-    {"name": "qwen3-32k:32b", "tag": "Secure — Accurate", "external": False},
-    {
-        "name": "qwen2.5-32k:72b",
-        "tag": "Secure — Fast",
-        "external": False,
-    },
+    {"name": "openai/gpt-4.1", "tag": "Cloud", "external": True},
+    {"name": "qwen3-32k:32b", "tag": "Private", "external": False},
 ]
 # debug(models)
 max_length = 120000 * 4  # 120K tokens, assuming 4 characters per token on average
