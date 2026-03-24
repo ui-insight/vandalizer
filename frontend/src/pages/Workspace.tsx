8fedecfa1792689523c1e7ce8685161ebe2967bc
ViaJables <johnbrunsfeld@gmail.com>
2026-03-24 09:57:35 -0700

    Remove pill from correct places

--- frontend/src/pages/Workspace.tsx
+++ frontend/src/pages/Workspace.tsx
@@ -1,10 +1,12 @@
 import { WorkspaceProvider } from '../contexts/WorkspaceContext'
 import { WorkspaceLayout } from '../components/workspace/WorkspaceLayout'
+import { CertificationPanel } from '../components/certification/CertificationPanel'
 
 export function Workspace() {
   return (
     <WorkspaceProvider>
       <WorkspaceLayout />
+      <CertificationPanel />
     </WorkspaceProvider>
   )
 }
